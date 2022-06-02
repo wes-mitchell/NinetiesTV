2e470f0fe35e7daf7e698e87219ce5c206bb4bbf
wes-mitchell <wmdrums@gmail.com>
2022-06-02 10:59:39 -0500

    completes prompt four

--- Program.cs
+++ Program.cs
@@ -64,7 +64,8 @@ namespace NinetiesTV
         // 4. Return a list of shows whose title contains an & character.
         static List<Show> ShowsWithAmpersand(List<Show> shows)
         {
-            throw new NotImplementedException();
+            
+            return shows.Where(s => s.Name.Contains('&')).ToList();
         }
 
         // 5. Return the most recent year that any of the shows aired.
